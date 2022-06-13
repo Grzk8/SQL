d50d1de1a7547fba1b101b137c03bd9934ca29e4
Grzk8 <grzesiekk125@gmail.com>
2022-06-13 22:02:18 +0200

    tables

---
+++ 02-create-users-table.sql
@@ -0,0 +1,9 @@
+-- postgresql -> enium najpierw ustalamy TYP
+CREATE TYPE employment_status AS ENUM ('employed', 'self-employed', 'unemployed');
+
+CREATE TABLE users (
+    full_name VARCHAR(200),
+    yearly_salary INT,
+    current_status employment_status
+    --current_status ENUM('employed', 'self-employed', 'unemployed') -- mysql syntax
+);
