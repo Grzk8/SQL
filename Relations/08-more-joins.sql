397fb9351baf1ea4ee8453525d41a47bec14de1b
Grzk8 <grzesiekk125@gmail.com>
2022-06-18 20:55:14 +0700

    foregin key

--- Relations/08-more-joins.sql
+++ Relations/08-more-joins.sql
@@ -1,4 +1,4 @@
-SELECT c.name AS city
+SELECT c.name AS city, u.first_name, u.last_name, a.street, a.house_number
 FROM cities AS c
 LEFT JOIN addresses AS a ON c.id = a.city_id
-INNER JOIN ;
+LEFT JOIN users AS u  ON u.address_id = a.id;
